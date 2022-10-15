361f199238a244d7bc6251891cc48a995a29aaa2
sbuhler3 <100046861+sbuhler3@users.noreply.github.com>
2022-10-14 23:14:44 -0600

    Delete Django directory

--- Django/django_projects/pokemon/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.get_pokemon, name='get_pokemon'),
-    path('pokemon/<int:id>/',views.pokemon_detail, name='pokemon_detail'),
-    path('<str:element>', views.type_chart, name='type_chart')
-]
