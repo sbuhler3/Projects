78d258087b306c7b1f45347a7ea5697c51e8f6b5
sbuhler3 <100046861+sbuhler3@users.noreply.github.com>
2022-10-22 11:57:38 -0600

    adding simple calculator

---
+++ React/simple_calculator/src/App.js
@@ -0,0 +1,70 @@
+import {
+  useState,
+  useRef
+} from "react"; 
+import "./App.css";
+
+function App() { 
+  const inputRef = useRef(null); 
+  const resultRef = useRef(null); 
+  const [result, setResult] = useState(0); 
+ 
+  function plus(e) { 
+    e.preventDefault(); 
+    setResult((result) => result + Number(inputRef.current.value)); 
+  }; 
+ 
+  function minus(e) { 
+  	e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
+  };
+ 
+  function times(e) { 
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));
+  }; 
+ 
+  function divide(e) { 
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value));
+  };
+ 
+  function resetInput(e) { 
+    e.preventDefault();
+    inputRef.current.value="";
+    setResult(inputRef.current);
+    
+  }; 
+ 
+  function resetResult(e) { 
+    resultRef.current.value="";
+    setResult(resultRef.current);
+  }; 
+ 
+  return ( 
+    <div className="App"> 
+      <div> 
+        <h1>Simple Working Calculator</h1> 
+      </div> 
+      <form> 
+        <p ref={resultRef}> 
+          {result} 
+        </p> 
+        <input
+          pattern="[0-9]" 
+          ref={inputRef} 
+          type="number" 
+          placeholder="Type a number" 
+        /> 
+        <button onClick={plus}>Add</button> 
+        <button onClick={minus}>Subtract</button> 
+        <button onClick={times}>Multiply</button> 
+        <button onClick={divide}>Divide</button> 
+        <button onClick={resetInput}>Clear Input</button> 
+        <button onClick={resetResult}>Clear Result</button> 
+      </form> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
