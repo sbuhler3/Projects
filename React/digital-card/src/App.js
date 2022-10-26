7e71549b7d4a8152c9cbdaa2b283bad3d5d6fd13
sbuhler3 <100046861+sbuhler3@users.noreply.github.com>
2022-10-26 10:35:52 -0600

    about section

--- React/digital-card/src/App.js
+++ React/digital-card/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Info from "./components/Info"
-
+import About from "./components/About"
 function App() {
   return (
     <div className="App">
     < Info />
+    < About/>
     </div>
   );
 }
---
+++ React/digital-card/src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function About() {
+    return (
+        <div className="about-row">
+            <h5 className="about">About</h5>
+            <div className="about-txt">I am a full stack developer with a particular interest
+                in creating efficient backends and responsive design for the
+                best user experience. I strive to stay up to date on best
+                current practice.
+            </div>
+        </div>
+    )
+}
