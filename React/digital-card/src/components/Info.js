01e2b0032ca50297336116ee78d3d56bc01a8461
sbuhler3 <100046861+sbuhler3@users.noreply.github.com>
2022-10-25 23:10:14 -0600

    email icon error

--- React/digital-card/src/components/Info.js
+++ React/digital-card/src/components/Info.js
@@ -12,7 +12,7 @@ export default function Info() {
         target="_blank">buhler-portfolio.herokuapp.com</a></div>
         <div className="button-group">
         <a href="mailto:buhler.spencer33@gmail.com">
-            <button className="email-but"><img alt="email icon"clesrc={email}/><span className="but-text">Email</span></button></a>
+            <button className="email-but"><img alt="email icon" src={email}/><span className="but-text">Email</span></button></a>
             <a href="https://www.linkedin.com/in/spencer-buhler-331623118/" target="_blank" >
             <button className="linked-but"><img alt="linkedin icon"src={linked}/><span className="but-text">LinkedIn</span></button></a>
         </div>
