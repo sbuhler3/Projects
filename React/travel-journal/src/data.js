d6372b4f394e271283222faa1b2bc83ef33c550b
sbuhler3 <100046861+sbuhler3@users.noreply.github.com>
2022-10-28 16:24:23 -0600

    data creation

---
+++ React/travel-journal/src/data.js
@@ -0,0 +1,36 @@
+export default [
+    {
+        id:1,
+        img: "edinburgh.png",
+        country: "Scotland",
+        googlelink: "https://www.google.com/maps/place/Edinburgh+Castle/@55.9485977,-3.2021075,17z/data=!3m1!4b1!4m5!3m4!1s0x4887c79a2099c0f7:0x469a1eebe54c0a58!8m2!3d55.9485947!4d-3.1999135",
+        location: "Edinburgh",
+        dates: "Summer 2017", 
+        description: `Physically, Edinburgh is a city of sombre theatricality, with much of this 
+        quality deriving from its setting among crags and hills and from its tall buildings and spires of 
+        dark stone. Edinburgh has been a military stronghold, the capital of an independent country, and a 
+        centre of intellectual activity.`
+    },
+    {
+        id:2,
+        img: "glacier.png",
+        country:"Montana",
+        googlelink: "https://www.google.com/maps/place/Glacier+National+Park/@48.6553552,-114.9936151,9z/data=!3m1!4b1!4m5!3m4!1s0x5368901555555555:0xaf16bc2215c55dec!8m2!3d48.7596128!4d-113.7870225",
+        location: "Glacier National Park",
+        dates: "Summer 2018",
+        description: `Glacier National Park is a 1,583-sq.-mi. wilderness area in Montana's Rocky 
+        Mountains, with glacier-carved peaks and valleys running to the Canadian border. It's crossed by the mountainous 
+        Going-to-the-Sun Road. Among more than 700 miles of hiking trails, it has a route to photogenic Hidden Lake. 
+        Other activities include backpacking, cycling and camping. Diverse wildlife ranges from mountain goats to grizzly bears.`
+    },
+    {id:3,
+    img:"phi_phi.png",
+    country:"Thailand",
+    googlelink:"https://www.google.com/maps/place/Phi+Phi+Viewpoint+3/@7.7628068,98.7176022,13.42z/data=!4m9!1m2!2m1!1sphi+phi+island+description!3m5!1s0x3051deef8adc409f:0x7c00a22d0477c53b!8m2!3d7.7490731!4d98.779707!15sChpwaGkgcGhpIGlzbGFuZCBkZXNjcmlwdGlvblocIhpwaGkgcGhpIGlzbGFuZCBkZXNjcmlwdGlvbpIBC3NjZW5pY19zcG904AEA",
+    location:"Phi Phi Island",
+    dates:"Summer 2012",
+    description:`Phi Phi comprises 6 small islands about 46 km south of Phuket. Fine sandy beaches 
+    give way to soaring limestone cliffs to form spectacular scenery. Add crystal-clear water, a refreshing lack of 
+    roads, plus a laidback lifestyle, and it's easy to see why Phi Phi is one of southern Thailand's most popular destinations.`
+}
+]
