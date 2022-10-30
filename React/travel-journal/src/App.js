1258c65a5ab67afe9b606acaa0994f1b3c8df035
sbuhler3 <100046861+sbuhler3@users.noreply.github.com>
2022-10-29 21:01:33 -0600

    adding in props

--- React/travel-journal/src/App.js
+++ React/travel-journal/src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import globe from './images/globe.png'
 import Card from './components/Card'
+import data from './data'
 function App() {
+// variable that creates an object with all the values mapped onto it//
+
+let cards= data.map(item => {
+  return (
+    <Card 
+    item={item} />
+  )
+})
   return (
     <div className="App">
       <nav className="navbar">
         <span className="globe"><img src={globe} alt="globe graphic"/></span>
         Spencer Buhler Travel Highlights
       </nav>
-      < Card />
+      {cards}
     </div>
   );
 }
--- React/travel-journal/src/components/Card.js
+++ React/travel-journal/src/components/Card.js
@@ -3,15 +3,17 @@ import icon from "../images/icon.png"
 export default function Card(props) {
     return(
        <div className="card-container">
-        <img src="#"/>
-        <div className="card-column">
-            <div className="country"> filler
-                <img src={icon}/>
-                <span className="googlelink">View on Google Maps</span>
+        <img className="image"src={require(`../images/${props.item.img}`)}/>
+        <div className="card-text">
+            <div className="country">
+                <img src={icon}/>{props.item.country}
+                <a href={props.item.googlelink} target="_blank"
+                className="googlelink">View on Google Maps</a>
                 </div>
-                <h2 classname="location">anywhere</h2>
-                <div className="dates">all the time</div>
-                <div className="description">lorem ipsum</div>
+                <h2 classname="location">{props.item.location}</h2>
+                <div className="dates">{props.item.dates}</div>
+                <div className="description">{props.item.description}</div>
+
         </div>
        </div>
     )
