544fdcca7624caf781b3e12465361fe160c916dd
sbuhler3 <100046861+sbuhler3@users.noreply.github.com>
2022-10-31 23:12:54 -0600

    styling and complete

--- React/travel-journal/src/components/Card.js
+++ React/travel-journal/src/components/Card.js
@@ -3,16 +3,16 @@ import icon from "../images/icon.png"
 export default function Card(props) {
     return(
        <div className="card-container">
-        <img className="image"src={require(`../images/${props.item.img}`)}/>
+        <img className="image"src={require(`../images/${props.item.img}`)} alt="location"/>
         <div className="card-text">
             <div className="country">
-                <img src={icon}/>{props.item.country}
+                <img src={icon} alt="location icon"/>{props.item.country}
                 <a href={props.item.googlelink} target="_blank"
                 className="googlelink">View on Google Maps</a>
-                </div>
-                <h2 classname="location">{props.item.location}</h2>
-                <div className="dates">{props.item.dates}</div>
-                <div className="description">{props.item.description}</div>
+            </div>
+            <h2 classname="location">{props.item.location}</h2>
+            <div className="dates">{props.item.dates}</div>
+            <div className="description">{props.item.description}</div>
 
         </div>
        </div>
