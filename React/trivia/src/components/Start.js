59810a95756ae0907ed81c4f9fb642950ac9bac3
sbuhler3 <100046861+sbuhler3@users.noreply.github.com>
2022-11-11 11:35:01 -0700

    typos

--- React/trivia/src/components/Start.js
+++ React/trivia/src/components/Start.js
@@ -4,8 +4,8 @@ export default function Start(props){
     return(
         <>
         <h1 className="title">Quizzical</h1>
-        <div className="description">Welcome to Quiziccal. 
-        Press start to play a trvia game of 5 random questions.</div>
+        <div className="description">Welcome to Quizzical. 
+        Press start to play a trivia game of 5 random questions.</div>
         <button className="start-game"
         onClick={props.beginGame}
         >Start</button>
