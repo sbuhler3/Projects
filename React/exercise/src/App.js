16007717bdc43d88fd68cbf3ed983d8dd535d75f
sbuhler3 <100046861+sbuhler3@users.noreply.github.com>
2022-12-02 15:36:04 -0700

    navbar

--- React/exercise/src/App.js
+++ React/exercise/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-import Login from './components/Login'
+import { useState } from "react";
+import "./App.css";
+import Login from "./components/Login";
+import Nav from "./components/Nav";
 
 function App() {
+  const [validUser, setValidUser] = useState(false);
+  const valid = () => setValidUser(!validUser);
   return (
     <div className="App">
-     <Login/>
+      {validUser ? (
+        <>
+          <Nav setValidUser={valid} /> <h1>valid user logged on</h1>
+        </>
+      ) : (
+        <Login setValidUser={valid} />
+      )}
     </div>
   );
 }
