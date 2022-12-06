04b2fc6673c6cba5107cb618226b414bf291b515
sbuhler3 <100046861+sbuhler3@users.noreply.github.com>
2022-12-06 16:20:36 -0700

    navbar style icons

--- React/exercise/src/components/NavBar/NavLinks.js
+++ React/exercise/src/components/NavBar/NavLinks.js
@@ -1,10 +1,23 @@
+import { BiDumbbell, BiRun, BiHome, BiLogOut } from "react-icons/bi";
 export default function NavLinks() {
   return (
     <ul className="navbar-items">
-      <li className="navbar-item">Home</li>
-      <li className="navbar-item">Exercise Log</li>
-      <li className="navbar-item">Types of exercises</li>
-      <li className="navbar-item">Log out</li>
+      <li className="navbar-item">
+        <BiHome size={20} className="icon" />
+        Home
+      </li>
+      <li className="navbar-item">
+        <BiDumbbell size={20} className="icon" />
+        Strength Log
+      </li>
+      <li className="navbar-item">
+        <BiRun size={20} className="icon" />
+        Cardio Log
+      </li>
+      <li className="navbar-item">
+        <BiLogOut size={20} className="icon" />
+        Log out
+      </li>
     </ul>
   );
 }
