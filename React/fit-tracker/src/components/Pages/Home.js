9276f71694195170547fc9e4f975911ac256bd4d
sbuhler3 <100046861+sbuhler3@users.noreply.github.com>
2022-12-17 12:20:29 -0700

    home page

--- React/fit-tracker/src/components/Pages/Home.js
+++ React/fit-tracker/src/components/Pages/Home.js
@@ -6,16 +6,27 @@ export default function Home() {
       <Nav />
       <div className="page-container">
         <div className="welcome-text">Welcome Spencer!</div>
-        <div className="heart-container">
-          <div className="heart-zone" style={{ backgroundColor: "green" }}>
-            Low HR: 90 bpm
+        <div className="heart-rate">
+          Based on your age your target heart rate zones are:
+          <div style={{ color: "rgb(127, 183, 103)" }}>
+            <strong> low target zone 90-110 bpm</strong>
           </div>
-          <div className="heart-zone" style={{ backgroundColor: "orange" }}>
-            Medium HR: 120 bpm
+          <div c style={{ color: "rgb(246 171 34 / 80%)" }}>
+            {" "}
+            <strong>moderate target zone 109-149 bpm </strong>
           </div>
-          <div className="heart-zone" style={{ backgroundColor: "red" }}>
-            High HR: 167 bpm
+          <div>
+            <strong style={{ color: "rgb(188 10 10 / 80%)" }}>
+              {" "}
+              high target zone 150-190 bpm
+            </strong>
           </div>
+        </div>
+        <div className="recommended">
+          ACSM recommends getting 150 mins of exercise at your moderate target
+          zone a week. Or 75 mins of vigorous exercise at your high target zone
+          a week. Exercise should then be followed up by 2-5 mins of cool down
+          at your low target zone.
         </div>
       </div>
     </>
