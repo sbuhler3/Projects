d229fc333617e5679abf653a62e8e5bf937f2d41
sbuhler3 <100046861+sbuhler3@users.noreply.github.com>
2023-01-21 14:16:06 -0700

    beginning of table log for strength

--- React/fit-tracker/client/src/components/Pages/Strength.js
+++ React/fit-tracker/client/src/components/Pages/Strength.js
@@ -1,14 +1,62 @@
 import React, { useState } from "react";
 import Nav from "../NavBar/Nav";
+import data from "../../mock-strength-data.json";
+import { nanoid } from "nanoid";
 export default function Strength() {
   const today = new Date();
   const [month, setMonth] = useState(
     today.toLocaleString("default", { month: "short" })
   );
+  const [records, setRecords] = useState(data);
+  const [addRecord, setAddRecord] = useState({
+    date: "",
+    exercise: "",
+    sets: "",
+    reps: "",
+    resistance: "",
+  });
+
+  const handleRecordChange = (e) => {
+    const fieldName = e.target.getAttribute("name");
+    const fieldValue = e.target.value;
+    const newRecordData = { ...addRecord, [fieldName]: fieldValue };
+
+    setAddRecord(newRecordData);
+  };
+  console.log(records);
+  const handleRecordSubmit = (e) => {
+    e.preventDefault();
+
+    const newRecord = {
+      id: nanoid(),
+      date: addRecord.date,
+      exercise: addRecord.exercise,
+      sets: addRecord.sets,
+      reps: addRecord.reps,
+      resistance: addRecord.resistance,
+    };
+    const newRecords = [...records, newRecord];
+    setRecords(newRecords);
+  };
+  //const monthDict = {
+  //  Jan: 1,
+  //  Feb: 2,
+  //  Mar: 3,
+  //  Apr: 4,
+  //  May: 5,
+  //  Jun: 6,
+  //  Jul: 7,
+  //  Aug: 8,
+  //  Sep: 9,
+  //  Oct: 10,
+  //  Nov: 11,
+  //  Dec: 12,
+  //};
 
   const handleClick = (e) => {
     setMonth(e.currentTarget.innerText);
   };
+
   return (
     <>
       <header>
@@ -55,7 +103,66 @@ export default function Strength() {
             </li>
           </ul>
         </nav>
-        <h1>{month} Strength page</h1>
+        <div className="table-container">
+          <h1>{month} Strength page</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Exercise</th>
+                <th>Sets</th>
+                <th>Reps</th>
+                <th>Resistance</th>
+              </tr>
+            </thead>
+            <tbody>
+              {records.map((record) => (
+                <tr>
+                  <td>{record.date}</td>
+                  <td>{record.exercise}</td>
+                  <td>{record.sets}</td>
+                  <td>{record.reps}</td>
+                  <td>{record.resistance}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <form onSubmit={handleRecordSubmit}>
+            <input
+              type="date"
+              required
+              name="date"
+              placeholder="Enter a date"
+              onChange={handleRecordChange}
+            />
+            <input
+              type="text"
+              required
+              name="exercise"
+              placeholder="Enter a exercise"
+              onChange={handleRecordChange}
+            />
+            <input
+              type="number"
+              name="sets"
+              placeholder="Enter sets"
+              onChange={handleRecordChange}
+            />
+            <input
+              type="number"
+              name="reps"
+              placeholder="Enter reps"
+              onChange={handleRecordChange}
+            />
+            <input
+              type="text"
+              name="resistance"
+              placeholder="Enter resistance"
+              onChange={handleRecordChange}
+            />
+            <button>Add row</button>
+          </form>
+        </div>
       </main>
     </>
   );
