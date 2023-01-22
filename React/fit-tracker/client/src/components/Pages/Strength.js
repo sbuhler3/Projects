87f143ae13189a69f0c308c017fe5deff40b6914
sbuhler3 <100046861+sbuhler3@users.noreply.github.com>
2023-01-21 21:25:56 -0700

    add sidebar component

--- React/fit-tracker/client/src/components/Pages/Strength.js
+++ React/fit-tracker/client/src/components/Pages/Strength.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Nav from "../NavBar/Nav";
 import data from "../../mock-strength-data.json";
 import { nanoid } from "nanoid";
+import Sidebar from "../NavBar/Sidebar";
 export default function Strength() {
   const today = new Date();
   const [month, setMonth] = useState(
@@ -38,22 +39,8 @@ export default function Strength() {
     const newRecords = [...records, newRecord];
     setRecords(newRecords);
   };
-  //const monthDict = {
-  //  Jan: 1,
-  //  Feb: 2,
-  //  Mar: 3,
-  //  Apr: 4,
-  //  May: 5,
-  //  Jun: 6,
-  //  Jul: 7,
-  //  Aug: 8,
-  //  Sep: 9,
-  //  Oct: 10,
-  //  Nov: 11,
-  //  Dec: 12,
-  //};
 
-  const handleClick = (e) => {
+  const handleClickMonth = (e) => {
     setMonth(e.currentTarget.innerText);
   };
 
@@ -63,46 +50,7 @@ export default function Strength() {
         <Nav />
       </header>
       <main className="page">
-        <nav>
-          <ul className="months">
-            <li className="month" onClick={handleClick}>
-              Jan
-            </li>
-            <li className="month" onClick={handleClick}>
-              Feb
-            </li>
-            <li className="month" onClick={handleClick}>
-              Mar
-            </li>
-            <li className="month" onClick={handleClick}>
-              Apr
-            </li>
-            <li className="month" onClick={handleClick}>
-              May
-            </li>
-            <li className="month" onClick={handleClick}>
-              Jun
-            </li>
-            <li className="month" onClick={handleClick}>
-              Jul
-            </li>
-            <li className="month" onClick={handleClick}>
-              Aug
-            </li>
-            <li className="month" onClick={handleClick}>
-              Sep
-            </li>
-            <li className="month" onClick={handleClick}>
-              Oct
-            </li>
-            <li className="month" onClick={handleClick}>
-              Nov
-            </li>
-            <li className="month" onClick={handleClick}>
-              Dec
-            </li>
-          </ul>
-        </nav>
+        <Sidebar handleClick={handleClickMonth} />
         <div className="table-container">
           <h1>{month} Strength page</h1>
           <table>
