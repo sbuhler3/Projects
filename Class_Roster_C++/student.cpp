dc3b2f9ffd2bd082fcb43855d72ad62245e25047
sbuhler3 <100046861+sbuhler3@users.noreply.github.com>
2023-02-20 16:14:16 -0700

    constructors

--- Class_Roster_C++/student.cpp
+++ Class_Roster_C++/student.cpp
@@ -1,3 +1,82 @@
 #include <iostream>
+#include "student.h"
 #include <string>
 using namespace std;
+
+//DEFAULT CONSTRUCTOR
+Student::Student(){
+    this->studentID=-1;
+    this->firstName="NoName";
+    this->lastName="NoName";
+    this->email="NoEmail";
+    this->age=-1;
+    for (int i=0; i<arraySize; ++i){
+        this->daysToComplete[i]=0;
+    }
+    this->degree = DegreeProgram::NETWORK;
+}
+//FULL CONSTRUCTOR
+Student::Student(int studentID, string firstName, string lastName, string email, int age, int daysToComplete[], DegreeProgram degree){
+    this-> studentID=studentID;
+    this-> firstName=firstName;
+    this-> lastName=lastName;
+    this-> email=email;
+    this-> age=age;
+    for (int i=0; i<arraySize; ++i){
+        this->daysToComplete[i]=daysToComplete[i];
+    }
+    this->degree=degree;
+ 
+}
+//DECONSTRUCTOR
+Student::~Student() {};
+
+//SETTING METHODS
+void Student::SetID(int studentID) {                
+    this->studentID=studentID;}
+
+void Student::SetFirstName(string firstName) {
+    this->firstName=firstName;}
+
+void Student::SetLastName(string lastName) {
+    this->lastName=lastName;}
+
+void Student::SetEmail(string email) {
+    this->email=email;}
+
+void Student::SetAge(int age) {
+    this->age=age;}
+
+//void Student::SetDaysToComplete(int age) {
+
+//GETTING METHODS
+int Student::GetID() const {
+    return studentID;
+}
+
+string Student::GetFirstName() const {
+    return firstName;
+}
+
+string Student::GetLastName() const {
+    return lastName;
+}
+
+string Student::GetEmail() const {
+    return email;
+}
+
+int Student::GetAge() const {
+    return age;
+}
+
+//PRINTING METHOD
+void Student::Print() const {
+    cout << "TESTING"<<studentID<<firstName<<lastName<<email<<age;}
+int main() {
+    Student testStudent;
+    testStudent.Print();
+
+   
+    return 0;
+}
--- Class_Roster_C++/student.h
+++ Class_Roster_C++/student.h
@@ -1,5 +1,53 @@
 #ifndef STUDENT_H
 #define STUDENT_H
+#include "degree.h"
+#include <string>
+using namespace std;
+
+
+class Student {
+    public: 
+    //const value for array
+    const static int arraySize=3;
+    //default constructor
+    Student();
+    //full constructor
+    Student(int studentID, string firstName, string lastName, string email, int age, int daysToComplete[], DegreeProgram degree);
+    //deconstructor
+    ~Student();
+
+    //Setting student attributes
+    void SetID(int studentID);                                
+    void SetFirstName(string firstName);
+    void SetLastName(string lastName);
+    void SetEmail(string email);
+    void SetAge(int age);
+    void SetDaysToComplete(const int daysToComplete[]);
+    void SetDegree(DegreeProgram deg);
+
+    //getting student attributes
+    int GetID() const;                                     
+    string GetFirstName() const;
+    string GetLastName() const;
+    string GetEmail() const;
+    int GetAge() const;
+    int *GetDaysToComplete() const;
+    DegreeProgram GetDegree() const; 
+
+    //printing student attributes
+    void Print() const;                                     
+
+
+    private:
+    int studentID;
+    string firstName;
+    string lastName;
+    string email;
+    int age;
+    int daysToComplete[arraySize];
+    DegreeProgram degree;
+
+};
 
 
 
